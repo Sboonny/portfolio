929ba6c535f9ab89e8c34cef06cb67db8a4fcbf9
Sboonny <muhammedelruby@gmail.com>
2023-11-04 17:39:51 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), prefetch()],
+  output: "server",
+  adapter: vercel()
 });
