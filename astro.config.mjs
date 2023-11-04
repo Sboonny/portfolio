3f58460742461bca61635415be0d1883017cc6ac
Sboonny <muhammedelruby@gmail.com>
2023-11-04 17:54:00 +0200

    Revert "add vercel adapter"
    This reverts commit 929ba6c535f9ab89e8c34cef06cb67db8a4fcbf9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), prefetch()],
-  output: "server",
-  adapter: vercel()
 });
